9787be2561dbf4245ef9f0c48c046451e6a624b9
cseljatib <cseljatib@gmail.com>
2025-08-28 13:42:14 -0400

    script sobre ajuste no lineal-altura

--- ajuhdnolineal1.r
+++ ajuhdnolineal1.r
@@ -8,7 +8,7 @@
 ##! -------------------------------------------------------/ 
 ##                                                        /
 ##> Profesor: Christian Salas Eljatib                    /
-## E-mail: christian.salas AT uchile DOT cl             /
+##? E-mail: christian.salas AT uchile DOT cl            /
 ## Web: https://eljatib.com                            /
 ##!===================================================/
 
